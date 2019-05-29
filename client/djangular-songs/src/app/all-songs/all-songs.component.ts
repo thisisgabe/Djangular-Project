0896056547d61a12e15e457f8b6cd9d079beb642
Gabe Velasquez <gvelasquez@gmail.com>
2019-05-29 09:56:28 -0700

    nav bar loading on page in AllSongsComponent

--- client/djangular-songs/src/app/all-songs/all-songs.component.ts
+++ client/djangular-songs/src/app/all-songs/all-songs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavBarComponent } from '../nav-bar/nav-bar.component'
 
 @Component({
   selector: 'app-all-songs',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-songs.component.scss']
 })
 export class AllSongsComponent implements OnInit {
+  userName: string;
+  userId: Number;
 
-  constructor() { }
+  constructor() { 
+    this.userName = 'Gabe';
+    this.userId = 1;
+  }
 
   ngOnInit() {
   }
--- client/djangular-songs/src/app/nav-bar/nav-bar.component.ts
+++ client/djangular-songs/src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  @Input() firstName: string;
+  @Input() userId: Number;
+
+  myName:string;
+
+  constructor() { 
+    this.myName = this.firstName;
+  }
 
   ngOnInit() {
   }
