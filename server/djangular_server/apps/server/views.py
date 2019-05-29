d38a1591bbc7618a3ad153d6e20c7e29321fc42f
Gabe Velasquez <gvelasquez@gmail.com>
2019-05-28 18:04:04 -0700

    added backend logic for registration

--- server/djangular_server/apps/server/views.py
+++ server/djangular_server/apps/server/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.core import serializers
+from .models import *
+import json
 
 # Create your views here.
+def create(req):
+    post_data = json.loads(req.body.decode())
+    errors = User.objects.validate(post_data)
+    if errors:
+        return HttpResponse(json.dumps(errors), status=400, content_type='application/json')
+    
+    # create a user, return user info as json
+    entire_user = User.objects.easy_create(post_data)
+    user = {
+        'first_name': entire_user.first_name,
+        'id': entire_user.id,
+        'gold': entire_user.gold
+    }
+    json_user = json.dumps(user)
+    return HttpResponse(json_user, status=200, content_type="application/json")
+
+def login(req):
+    post_data = json.loads(req.body.decode())
+    valid, result = User.objects.login(post_data)
+    if not valid:
+        json_errors = json.dumps(result)
+        return HttpResponse(json_errors, status=400, content_type="application/json")
+    user = {
+        'first_name': result.first_name,
+        'id': result.id,
+        'gold': result.gold
+    }
+    json_user = json.dumps(user)
+    return HttpResponse(json_user, status=200, content_type='application/json')
